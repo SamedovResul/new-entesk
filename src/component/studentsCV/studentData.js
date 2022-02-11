819651a0e888400fa4fe3806a442b22a44bbdfe5
SamedovRasul <samedovrasul7@gmail.com>
2022-02-11 16:44:45 +0400

    new student cv

--- src/component/studentsCV/studentData.js
+++ src/component/studentsCV/studentData.js
@@ -4,5 +4,11 @@ export const studentData = [
     frontImg:"/img/imranfront.png",
     backImg:"/img/imranback.jpg",
     resBackImg:"/img/imranbackphone.jpg"
+  },
+  {
+    studentName:"Oqtay",
+    frontImg:"/img/oqtayfront.png",
+    backImg:"/img/oqtayback.jpg",
+    resBackImg:"/img/oqtaybackphone.jpg"
   }
 ]
