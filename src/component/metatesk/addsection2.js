45e4a468c4108ff0d602a62cab5c150418c80b2b
SamedovRasul <samedovrasul7@gmail.com>
2022-10-10 16:08:34 +0400

    add srift

--- src/component/metatesk/addsection2.js
+++ src/component/metatesk/addsection2.js
@@ -9,21 +9,28 @@ const Addsection2 = () => {
       <div  className="container">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-4">
-              <div className="img-box">
-                <img src={img1} alt="metatesk" />
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="img-box">
-                <img src={img2} alt="metatesk" />
-
+            <div className="col-md-12">
+              <div className="text-box">
+               <h3> accessible by any device </h3>
+               <h3> for your convenience </h3>
               </div>
             </div>
             <div className="col-md-4">
               <div className="img-box">
                 <img src={img3} alt="metatesk" />
-
+                <p>PC</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="img-box">
+                <img src={img2} alt="metatesk" />
+                <p>VR</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="img-box">
+                <img src={img1} alt="metatesk" />
+                <p>MOBILE</p>
               </div>
             </div>
           </div>
